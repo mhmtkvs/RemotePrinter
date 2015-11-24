e78b15ea4609170e40f3f2f000ce5334d9e497da
Veli BAYAR <bayar.veli@gmail.com>
2015-11-24 13:48:18 +0200

    Menü eklendi.
    Form adı eklendi.

--- Remote Printer/Form1.Designer.cs
+++ Remote Printer/Form1.Designer.cs
@@ -28,20 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menüToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hakkındaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yazıcıEkleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yazıcıKaldırToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menüToolStripMenuItem,
+            this.hakkındaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(610, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menüToolStripMenuItem
+            // 
+            this.menüToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yazıcıEkleToolStripMenuItem,
+            this.yazıcıKaldırToolStripMenuItem});
+            this.menüToolStripMenuItem.Name = "menüToolStripMenuItem";
+            this.menüToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menüToolStripMenuItem.Text = "Menü";
+            // 
+            // hakkındaToolStripMenuItem
+            // 
+            this.hakkındaToolStripMenuItem.Name = "hakkındaToolStripMenuItem";
+            this.hakkındaToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.hakkındaToolStripMenuItem.Text = "Hakkında";
+            // 
+            // yazıcıEkleToolStripMenuItem
+            // 
+            this.yazıcıEkleToolStripMenuItem.Name = "yazıcıEkleToolStripMenuItem";
+            this.yazıcıEkleToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.yazıcıEkleToolStripMenuItem.Text = "Yazıcı Ekle";
+            // 
+            // yazıcıKaldırToolStripMenuItem
+            // 
+            this.yazıcıKaldırToolStripMenuItem.Name = "yazıcıKaldırToolStripMenuItem";
+            this.yazıcıKaldırToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.yazıcıKaldırToolStripMenuItem.Text = "Yazıcı Kaldır";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(479, 377);
+            this.ClientSize = new System.Drawing.Size(610, 554);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Remote Printer v1.0";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menüToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yazıcıEkleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yazıcıKaldırToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hakkındaToolStripMenuItem;
     }
 }
 
